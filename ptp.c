e62db034cea2ccbdc5ce759f54a95b06a3b58137
Teis Johansen <tejo@bang-olufsen.dk>
2020-02-21 13:19:42 +0100

    Extract packet timestamps

--- ptp.c
+++ ptp.c
@@ -1,5 +1,13 @@
 #include "mai.h"
 
+#include <sys/ioctl.h>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <linux/net_tstamp.h>
+#include <linux/sockios.h>
+
 /* ######################################################################## */
 struct packet {
         uint8_t         type;
@@ -49,6 +57,102 @@ static uint64_t ptp_stamp(uint8_t *in) {
 }
 
 /* ######################################################################## */
+static uint64_t ptp_timespec_to_ts(const struct timespec *ts) {
+#define NS_PER_SEC 1000000000ULL
+	return (uint64_t)ts->tv_sec * NS_PER_SEC + (uint64_t)ts->tv_nsec;
+}
+
+/* ######################################################################## */
+enum ptp_ts_type { PTP_NO_TS, PTP_HARDWARE_TS, PTP_SOFTWARE_TS };
+
+static enum ptp_ts_type ptp_get_ts(struct msghdr *msg, uint64_t *ts)
+{
+	struct timespec *sw = NULL;
+	struct timespec *hw = NULL;
+
+	for (struct cmsghdr *cm = CMSG_FIRSTHDR(msg); cm != NULL;
+	     cm = CMSG_NXTHDR(msg, cm)) {
+		if (cm->cmsg_level != SOL_SOCKET)
+			continue;
+
+		switch (cm->cmsg_type) {
+		case SCM_TIMESTAMPING:
+			if (cm->cmsg_len < sizeof(*hw) * 3) {
+				mai_error("short SO_TIMESTAMPING message");
+				continue;
+			}
+			hw = (struct timespec *)CMSG_DATA(cm);
+			break;
+		case SCM_TIMESTAMPNS:
+			if (cm->cmsg_len < sizeof(*sw)) {
+				mai_error("short SO_TIMESTAMPING message");
+				continue;
+			}
+			sw = (struct timespec *)CMSG_DATA(cm);
+			break;
+		}
+	}
+
+	if (hw) {
+		mai_debug("hw: [%luns, %luns, %luns]\n",
+			  ptp_timespec_to_ts(&hw[0]),
+			  ptp_timespec_to_ts(&hw[1]),
+			  ptp_timespec_to_ts(&hw[2]));
+		*ts = ptp_timespec_to_ts(&hw[2]);
+		return PTP_HARDWARE_TS;
+	} else if (sw) {
+		*ts = ptp_timespec_to_ts(sw);
+		mai_debug("sw: %luns\n", *ts);
+		return PTP_SOFTWARE_TS;
+	} else {
+		return PTP_NO_TS;
+	}
+}
+
+/* ######################################################################## */
+static int ptp_sendmsg(int fd, void *buf, size_t len, uint64_t *ts) {
+	int res = send(fd, buf, len, 0);
+	if (res <= 0)
+		return res;
+
+	struct pollfd pfd = { .fd = fd, .events = POLL_PRI };
+	res = poll(&pfd, 1, 1);
+	switch (res) {
+	case -1:
+		mai_error("poll: %m\n");
+		return res;
+	case 0:
+		mai_error("timed out waiting for tx timestamp\n");
+		return res;
+	default:
+		if (!(pfd.revents & POLL_PRI)) {
+			mai_error("poll woke up on non-error event\n");
+			return -1;
+		}
+		break;
+	}
+
+	char control[256] = { 0 };
+	struct iovec iov = { .iov_base = buf, .iov_len = len };
+	struct msghdr msg = { 0 };
+
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = control;
+	msg.msg_controllen = sizeof(control);
+
+	res = recvmsg(fd, &msg, MSG_ERRQUEUE);
+	if (res < 0) {
+		mai_error("recvmsg: %m\n");
+		return res;
+	}
+
+	// TODO(tejo): Just return `ts_type` (requires changes to caller)
+	enum ptp_ts_type ts_type = ptp_get_ts(&msg, ts);
+	return (ts_type == PTP_NO_TS) ? 0 : res;
+}
+
+/* ######################################################################## */
 static void ptp_update(void) {
 	// send delay requests only in sender mode and only every 2 seconds
 	if (!MAI_SENDER || (req_sync > ptp_sync) || ((ptp_sync - req_sync) < (ptp_rate * 2)))
@@ -67,11 +171,36 @@ static void ptp_update(void) {
 	packet->length   = pktlen;		// PTP: Header + Body Length
 	packet->sequence = ++req_seq;		// PTP: Expected Response Sequence
 	
-	if ((send(req_sock, packet, pktlen, 0)) <= 0)
+	uint64_t pck_sent;
+	if ((ptp_sendmsg(req_sock, packet, pktlen, &pck_sent)) <= 0)
 		mai_error("send: %m\n");
 		
 	req_sent = mai_rtp_clock();		// set delay request time (T2)
+	mai_debug("pck_sent: %lu, req_sent: %lu\n", pck_sent, req_sent);
 	MAI_STAT_INC(ptp.requests);
+}
+
+/* ######################################################################## */
+static int ptp_recvmsg(int fd, void *buf, size_t len, uint64_t *ts)
+{
+	char control[256] = { 0 };
+	struct iovec iov = { .iov_base = buf, .iov_len = len };
+	struct msghdr msg = { 0 };
+
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = control;
+	msg.msg_controllen = sizeof(control);
+
+	int cnt = recvmsg(fd, &msg, 0);
+	if (cnt < 0) {
+		mai_error("recvmsg: %m\n");
+		return cnt;
+	}
+
+	// TODO(tejo): Just return `ts_type` (requires changes to caller)
+	enum ptp_ts_type ts_type = ptp_get_ts(&msg, ts);
+	return (ts_type == PTP_NO_TS) ? 0 : cnt;
 }
 
 /* ######################################################################## */
@@ -83,7 +212,8 @@ static void *ptp_general(void *arg) {
 	struct packet *packet = (struct packet *)data;
 	
 	for (ssize_t r; 1; ) {
-		if ((r = recv(gen_sock, data, sizeof(data), 0)) <= 0)
+		uint64_t pck_recv;
+		if ((r = ptp_recvmsg(gen_sock, data, sizeof(data), &pck_recv)) <= 0)
 			mai_error("recv: %m\n");
 			
 		if (((packet->version & 0x0F) != 2) || (packet->domain != 0))
@@ -99,7 +229,7 @@ static void *ptp_general(void *arg) {
 				
 			ptp_recv = clk_recv;			// set received time (T'1)
 			ptp_sync = ptp_stamp(packet->payload);	// set master time   (T1)
-			
+			mai_debug("pck_recv: %lu, ptp_recv: %lu, ptp_sync: %lu\n", pck_recv, ptp_recv, ptp_sync);
 			ptp_update();
 			
 		} else if (type == 0x09) { 			// is this a delay response message?
@@ -107,9 +237,12 @@ static void *ptp_general(void *arg) {
 				continue;
 				
 			req_sync = ptp_stamp(packet->payload);	// set master delay (T'2)
-			
+
+			int64_t offset = ((int64_t)ptp_recv - (int64_t)ptp_sync - (int64_t)req_sync + (int64_t)req_sent) / 2;
+			mai_debug("pck_recv: %luns, req_sync: %luns, offset = %ldns\n", pck_recv, req_sync, offset);
+
 			// send calculated PTP offset to RTP system
-			mai_rtp_offset(((int64_t)ptp_recv - (int64_t)ptp_sync - (int64_t)req_sync + (int64_t)req_sent) / 2);
+			mai_rtp_offset(offset);
 		}
 	}
 
@@ -133,7 +266,8 @@ static void *ptp_event(void *arg) {
 	
 	// receive packet loop
 	for (ssize_t r; 1; ) {
-		if ((r = recv(ptp_sock, data, sizeof(data), 0)) <= 0)
+		uint64_t pck_recv;
+		if ((r = ptp_recvmsg(ptp_sock, data, sizeof(data), &pck_recv)) <= 0)
 			mai_error("recv: %m\n");
 			
 		if (((packet->version & 0x0F) != 2) || (packet->domain != 0))
@@ -170,11 +304,11 @@ static void *ptp_event(void *arg) {
 		if (packet->flags & flag_two_step) {	// is this a two-phase clock?
 			clk_seq  = packet->sequence;	// save sequence
 			clk_recv = mai_rtp_clock();	// save received time
-
+			mai_debug("pck_recv: %lu, clk_recv: %lu, clk_seq: %u\n", pck_recv, clk_recv, clk_seq);
 		} else {				// otherwise, it's a single phase clock
 			ptp_recv = mai_rtp_clock();	// set received time
 			ptp_sync = stamp;		// set master time
-			
+			mai_debug("pck_recv: %lu, ptp_recv: %lu, ptp_sync: %lu\n", pck_recv, ptp_recv, ptp_sync);
 			ptp_update();
 		}
 	}
@@ -184,17 +318,60 @@ static void *ptp_event(void *arg) {
 }
 
 /* ######################################################################## */
+static int ptp_hwts_init(int fd) {
+	struct hwtstamp_config cfg = { .flags = 0,
+				       .tx_type = HWTSTAMP_TX_ON,
+				       .rx_filter = HWTSTAMP_FILTER_SOME };
+	struct ifreq ifreq = { .ifr_data = (void *)&cfg };
+	strncpy(ifreq.ifr_ifrn.ifrn_name, mai_sock_if_name(), sizeof(ifreq.ifr_ifrn.ifrn_name) - 1);
+	return ioctl(fd, SIOCSHWTSTAMP, &ifreq);
+}
+
+/* ######################################################################## */
+static int ptp_sock_open(int mode, const char *ip, const uint16_t port) {
+	int sock = mai_sock_open(mode, ip, port);
+	if (sock < 0)
+		return sock;
+
+	if (ptp_hwts_init(sock) < 0) {
+		mai_error("could not enable hardware timestamping (%s)\n", strerror(errno));
+		goto bad_sock;
+	}
+
+	int flags = SOF_TIMESTAMPING_TX_HARDWARE |
+		    SOF_TIMESTAMPING_RX_HARDWARE |
+		    SOF_TIMESTAMPING_RAW_HARDWARE;
+
+	if (setsockopt(sock, SOL_SOCKET, SO_TIMESTAMPING, &flags, sizeof(flags)) < 0) {
+		mai_error("could not set SO_TIMESTAMPING option (%s)\n", strerror(errno));
+		goto bad_sock;
+	}
+
+	int on = 1;
+	if (setsockopt(sock, SOL_SOCKET, SO_TIMESTAMPNS, &on, sizeof(on)) < 0) {
+		mai_error("could not set SO_TIMESTAMPNS option (%s)\n", strerror(errno));
+		goto bad_sock;
+	}
+
+	return sock;
+
+bad_sock:
+	close(sock);
+	return(-1);
+}
+
+/* ######################################################################## */
 static pthread_t evt_tid;
 static pthread_t gen_tid;
 
 int mai_ptp_init(void) {
-	if ((ptp_sock = mai_sock_open('r', "224.0.1.129", 319)) < 0)
+	if ((ptp_sock = ptp_sock_open('r', "224.0.1.129", 319)) < 0)
 		return(mai_error("could not open PTP event socket\n"));
 		
-	if ((gen_sock = mai_sock_open('r', "224.0.1.129", 320)) < 0)
+	if ((gen_sock = ptp_sock_open('r', "224.0.1.129", 320)) < 0)
 		return(mai_error("could not open PTP general socket\n"));
 		
-	if ((req_sock = mai_sock_open('s', "224.0.1.129", 319)) < 0)
+	if ((req_sock = ptp_sock_open('s', "224.0.1.129", 319)) < 0)
 		return(mai_error("could not open PTP message socket\n"));
 		
 	return(mai_debug("PTP Domain: 224.0.1.129 (0)\n"));
